c21ae9d22a411f17e7073142ca4b6d2721dfe5b1
SantiagoHrd <santiagohrd1@gmail.com>
2025-05-17 17:22:21 -0500

    Add the put, post, delete methods

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 // Importar el modulo express
 const express = require('express');
 
+const bodyParser = require('body-parser');
 const mongodb = require('./data/contacts');
 
-// Se crea una instancia de una aplicación Express
-// app representa el servidor y es el que se usará para configurar rutas
 const app = express()
-
-// Define el puerto en el que correrá el servidor
-// El puerto 3000 es el puerto predeterminado para los servidores de desarrollo locales
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
 
@@ -19,7 +16,6 @@ mongodb.initDb((err) => {
         console.log(err)
     }
     else {
-        // inicia el servidor y lo pone a escuchar peticiones en el puerto especificado
         app.listen(port, () => (console.log(`Dabase is listening and node Running on port ${port}`)));
     }
 });
